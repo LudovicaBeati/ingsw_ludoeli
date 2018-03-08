dc8da1866043f5fae7e8c6e9bf8d8710da4a4bb3
LudovicaBeati <37111464+LudovicaBeati@users.noreply.github.com>
2018-03-08 13:16:13 +0100

    Add files via upload

--- INGSW_ChildCareTech/src/application/MenuInizialeController.java
+++ INGSW_ChildCareTech/src/application/MenuInizialeController.java
@@ -1,9 +1,24 @@
 package application;
 
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
 public class MenuInizialeController {
     public void bambini() {
-        System.out.println("ciao");
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Bambini.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene (root1));
+            stage.show();
+        }catch (Exception e){
+            System.out.println("error");
+        }
 
     }
 
