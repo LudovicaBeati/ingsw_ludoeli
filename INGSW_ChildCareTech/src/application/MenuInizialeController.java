9f26d94d8c07729c0c414b5f7372bb6020195d3e
LudovicaBeati <37111464+LudovicaBeati@users.noreply.github.com>
2018-03-08 17:20:51 +0100

    Add files via upload

--- INGSW_ChildCareTech/src/application/MenuInizialeController.java
+++ INGSW_ChildCareTech/src/application/MenuInizialeController.java
@@ -4,21 +4,63 @@ package application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import javax.lang.model.AnnotatedConstruct;
+import java.time.LocalDate;
+
 
 public class MenuInizialeController {
-    public void bambini() {
+
+    public void fornitore(){
         try{
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Bambini.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Fornitore.fxml"));
+
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene (root1));
             stage.show();
+
         }catch (Exception e){
             System.out.println("error");
         }
+
+    }
+
+    public void bambini() {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Bambini.fxml"));
+
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene (root1));
+            stage.show();
+
+        }catch (Exception e){
+            System.out.println("error");
+        }
+
+    }
+
+    public void PersonaleInterno(){
+
+       try{ FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PersonaleInterno.fxml"));
+
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene (root1));
+        stage.show();
+
+    }catch (Exception e){
+        System.out.println("error");
+    }
+
+    }
+
+    public void ReturnToHomePage(){
 
     }
 
